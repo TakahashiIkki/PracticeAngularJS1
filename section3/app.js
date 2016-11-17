4c9e65777dce15070e9400a797ff86be86b8fc29
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-11-18 08:32:08 +0900

    $scope, インターポレーションについて

--- section3/app.js
+++ section3/app.js
@@ -5,19 +5,12 @@
 /**
  *  angular というグローバル変数が定義される。
  *  第二引数には、依存するモジュールを定義する。
- *
- *  ngMessagesの依存性を注入する。
+ *。
  */
-var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+var myApp = angular.module('myApp', []);
 
-// $log: ログサービスを使う。
-// $resource:  ngResourceサービスを使う
-myApp.controller('mainController', function($scope, $log, $filter, $resource) {
+myApp.controller('mainController', function($scope) {
 
-    $scope.name = 'ken';
-    $scope.upperName = $filter('uppercase')($scope.name);
-
-    $log.info($scope.name);
-    $log.info($scope.upperName);
+    $scope.name = 'ケン';
 
 });
