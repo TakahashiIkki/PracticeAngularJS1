a856974cb7d179ea1693324872ffcbde6599a5fb
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-11-18 00:25:45 +0900

    Angularjs のサービスを使ってみるサンプル。

--- section3/app.js
+++ section3/app.js
@@ -5,11 +5,14 @@
 /**
  *  angular というグローバル変数が定義される。
  *  第二引数には、依存するモジュールを定義する。
+ *
+ *  ngMessagesの依存性を注入する。
  */
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 // $log: ログサービスを使う。
-myApp.controller('mainController', function($scope, $log, $filter) {
+// $resource:  ngResourceサービスを使う
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
 
     $scope.name = 'ken';
     $scope.upperName = $filter('uppercase')($scope.name);
