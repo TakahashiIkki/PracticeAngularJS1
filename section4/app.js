be2c5d4646a47bd1dcc9d352c9bbdb457a817d8d
Ikki Takahashi <new_ittyandesu@outlook.jp>
2017-05-14 15:29:12 +0000

    ng-if と ng-show

--- section4/app.js
+++ section4/app.js
@@ -9,41 +9,10 @@
  */
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
     $scope.handle = '';
     $scope.lowercaseHandle = function () {
         return $filter('lowercase')($scope.handle);
     };
-    
-    $scope.$watch('handle', function(newValue, oldValue) {
-        console.log('変化がありました');
-        console.log('新：' + newValue);
-        console.log('旧：' + oldValue);
-    
-    });
-
-    // // AngularJsはこのSettimeOut内で起きた事をハンドリングしていない。
-    // setTimeout(function() {
-    //     $scope.handle = 'hogehogehoge';
-    //     console.log('スコープが変わりました。');
-    // }, 3000);
-
-    // // Timeoutの解決方法１
-    // setTimeout(function() {
-        
-    //     // Apply関数の中で実行する。
-    //     // -> 普段書く必要は無いが、angularにハンドリングしてほしい為、明示的に記述。
-    //     $scope.$apply(function() {
-    //         $scope.handle = 'hogehogehoge';
-    //         console.log('スコープが変わりました。');
-    //     });
-    // }, 3000);
-    
-    // Timeoutの解決方法2
-    // Timeoutのサービスを使用する。
-    $timeout(function() {
-        $scope.handle = 'hogehogehoge';
-        console.log('スコープが変わりました。');
-    }, 3000);
 }]);
