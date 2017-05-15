f0dba5ad394bb3224edb02f616b8423c76e1bb81
Ikki Takahashi <new_ittyandesu@outlook.jp>
2017-05-15 14:54:44 +0000

    ng-repeatの練習

--- section4/app.js
+++ section4/app.js
@@ -12,8 +12,17 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
     $scope.handle = '';
-    $scope.characters = 5;
     $scope.lowercaseHandle = function () {
         return $filter('lowercase')($scope.handle);
     };
+    
+    $scope.characters = 5;
+    
+    $scope.rules = [
+        {rulename: '5文字でなければいけません'},
+        {rulename: 'まだ使われていないものでなければいけません'},
+        {rulename: '格好いいハンドルネームにしてください'},
+    ];
+    
+    console.log($scope.rules);
 }]);
