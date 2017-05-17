bbe470dfbaca7cc8355e8ac6d7e288480321c835
Ikki Takahashi <new_ittyandesu@outlook.jp>
2017-05-17 14:56:39 +0000

    ２つのコントローラー

---
+++ section5/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ikkitakahashi on 2016/11/13.
+ */
+
+/**
+ *  angular というグローバル変数が定義される。
+ *  第二引数には、依存するモジュールを定義する。
+ *。
+ */
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', function($scope) {
+
+    $scope.name = 'Main';
+    
+}]);
+
+myApp.controller('secondController', ['$scope', function($scope) {
+
+    $scope.name = 'Second';
+    
+}]);
